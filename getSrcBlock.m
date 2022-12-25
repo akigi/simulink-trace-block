666093c1d2fec531056ad74463de9a953cb8853d
akigi <akipon.dev@gmail.com>
2022-12-26 03:31:36 +0900

    update

--- getSrcBlock.m
+++ getSrcBlock.m
@@ -1,6 +1,7 @@
 function [srcBlockHandle, srcPortNumber] = getSrcBlock(target_block, target_inport_number)
 % ブロックのX番目の入力ポートに接続されている信号が、
 % どのブロックのどのポートから出力された信号なのか調べる関数。
+% 線が接続されていない場合、ともに-1を出力します。
 
 %target_block = gcb;
 %target_inport_number = 1;
@@ -12,10 +13,19 @@ if length(lines.Inport) < target_inport_number
 end
 line = lines.Inport(target_inport_number);
 
-% get source port handle
-line_srcport_h = get_param(line, 'SrcPortHandle');
+if line == -1
+    
+    fprintf('Warning: Inport %d of ''%s'' has no line connected.\n', ...
+                    target_inport_number, ...
+                    getfullname(target_block) ...
+    )
+    srcBlockHandle = -1;
+    srcPortNumber = -1;
+else
+    % get source port handle
+    line_srcport_h = get_param(line, 'SrcPortHandle');
 
-% get src block and port number
-srcBlockHandle = get_param(line_srcport_h, 'Parent');
-srcPortNumber = get_param(line_srcport_h, 'PortNumber');
-
+    % get src block and port number
+    srcBlockHandle = get_param(line_srcport_h, 'Parent');
+    srcPortNumber = get_param(line_srcport_h, 'PortNumber');
+end
