fd306be0fb46b5c77893d809e3a8f95c5daa9a12
akigi <akipon.dev@gmail.com>
2022-12-26 03:00:45 +0900

    update

--- traceBlock2.m
+++ traceBlock2.m
@@ -1,70 +1,67 @@
-function route = traceBlock2(sys, blk, options)
+function children = traceBlock2(sys, blk, parent)
+disp('--traceBlock2--')
 
 % sys = gcs
 % blk = gcb
 
-if options.first
-    options.route = struct('name', '', 'children', [])
-    options.first = 0
-    options.count = 0
-end
+blk_name = get_param(blk, 'Name');
+blk_h = get_param(blk, 'Handle');
+disp(['対象:' blk_name]);
 
-r = struct('name', '', 'children', []);
-blk_name = get_param(blk. 'Name');
-r.name = blk_name;
-
-options.count = options.count + 1
+% ハイライト
+%hilite_system(blk_h);
 
 % open system if not opened
 if ~bdIsLoaded(sys)
     sys_h = load_system(sys);
     sys_name = get_param(sys_h, "Name");
+    disp([sys_name 'was loaded.']);
 end
 
-lines = get_param(blk, 'LineHandles');
 pc = get_param(blk, 'PortConnectivity');
+
+children = [];
+traced_inports = [];
 
 for i=1:length(pc)
     % 接続元ブロックを取得
     pc_field = pc(i);
     srcblk = pc_field.SrcBlock;
-    if ~srcblk
-        continue;
+    if ~any(size(srcblk))
+        disp('skip')
+        continue
     end
-    srcblk_name = get_param(srcblk, 'Name')
+
+    srcblk_h = get_param(srcblk, 'Handle');
+    srcblk_name = get_param(srcblk, 'Name');
+    srcblk_sys = get_param(srcblk, 'Parent');
+    srcblk_sys_isroot = ~contains(string(srcblk_sys), "/");
+    srcblk_blocktype = get_param(srcblk, 'BlockType');
+    srcblk_blocktype_isinport = strcmp(srcblk_blocktype, 'Inport');
+    srcblk_blocktype_issubsystem = strcmp(srcblk_blocktype, 'SubSystem');
+    
+    disp(srcblk_blocktype )
+    
+
     r_1 = struct('name', '', 'children', []);
     r_1.name = srcblk_name;
+    disp(['接続元:' r_1.name]);    
     
-    r.children = [r.children; r_1];
-    
-    % 接続元ブロックの種別により、中に入るか、中身を無視するか、を決める。
-    % サブシステム
-    srcblk_blocktype = get_param(srcblk, 'BlockType')
-    if strcmp(srcblk_blocktype, 'Subsystem')
-        % repeat
+    if srcblk_blocktype_issubsystem
+        % subsystem
+        % get line
+        lines = get_param(blk, 'LineHandles')
+        line = lines.Inport(i);
     else
-        traceBlock(srcblk_srcblk_sys, srcblk_srcblk_h, options);    
-        
-%     else
-%         srcblk_pc = get_param(srcblk, 'PortConnectivity')
-%         for j=1:length(srcblk_pc)
-%             srcblk_pc_field = srcblk_pc(j);
-%             srcblk_srcblk = srcblk_pc_field.SrcBlock;
-%             if srcblk_srcblk    
-%                 srcblk_srcblk_name = get_param(srcblk_srcblk, 'Name')
-%                 srcblk_srcblk_sys =  get_param(srcblk_srcblk, 'Parent')
-%                 srcblk_srcblk_path =  [srcblk_srcblk_sys  '/' srcblk_srcblk_name]
-%                 srcblk_srcblk_h =  get_param(srcblk_srcblk, 'Handle')
-%                 
-%                 traceBlock(srcblk_srcblk_sys, srcblk_srcblk_h, options);
-%                 
-%                 % repeae
-%             end
-%             
-%         end
+        r_1_children = traceBlock2(srcblk_sys, srcblk_h, r_1);
+    end
+    r_1.children = r_1_children;
+    
+    children = [children; r_1];
+
+    if srcblk_sys_isroot && srcblk_blocktype_isinport
+        traced_inports = [traced_inports; srcblk_h]
     end
 end
 
-
-%route = options.route
-route = options
+% children = parent.children;
