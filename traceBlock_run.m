f5b20e5645fb44f93f3fe75dd17716a9b60e67c6
akigi <akipon.dev@gmail.com>
2022-12-26 05:04:45 +0900

    traceBlock3.mの内容をtraceBlock.mにコピー。

--- traceBlock_run.m
+++ traceBlock_run.m
@@ -1,9 +1,6 @@
 clc;
 
-blk = find_system(bdroot(gcs),'type','block'); % モデル内のブロックをリストします
-for n = 1:length(blk) % 取得したブロックリスト数で繰り返し
-    hilite_system(blk{n},'none'); % 対象ブロックのhighlightをクリア
-end
+unhilite_gcs;
 
 r = struct('name', get_param(gcb, 'Name'), 'children', []);
 t = []
